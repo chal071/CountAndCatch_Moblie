bac9bf41e59b9f22630103127f692a350f95556f
JuliaSchmid2425 <jschmidp2425@politecnics.barcelona>
2025-11-04 17:17:47 +0100

    creado actividad resultado

---
+++ app/src/main/java/com/example/playandpaint/Resultado.kt
@@ -0,0 +1,21 @@
+package com.example.playandpaint
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat.enableEdgeToEdge
+import androidx.core.view.WindowInsetsCompat
+
+class Resultado {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_resultado)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.resultado)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
