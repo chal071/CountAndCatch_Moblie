712eb55dbc71f46363c19be71474b148ccd74638
WuQiaoq <qwu2324@politecnics.barcelona>
2025-11-11 17:27:40 +0100

    COMMIT acaba el diseño

--- app/src/main/java/com/example/countandcatch/ElegirJuegosActivity.kt
+++ app/src/main/java/com/example/countandcatch/ElegirJuegosActivity.kt
@@ -1,13 +1,22 @@
 package com.example.countandcatch
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+
 import androidx.core.view.WindowInsetsCompat
 
 
 class ElegirJuegosActivity : AppCompatActivity() {
+    private lateinit var btnEmpezar: Button;
+    private lateinit var frameJuegoCount: FrameLayout;
+    private lateinit var frameJuegoCatch: FrameLayout;
+    private var selectedJuego: String? = null //guardar el juego seleccionado
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +26,32 @@ class ElegirJuegosActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        frameJuegoCatch = findViewById(R.id.frameJuegoCatch)
+        frameJuegoCount = findViewById(R.id.frameJuegoCount)
+        btnEmpezar = findViewById(R.id.btnEmpezar)
+
+        frameJuegoCount.setOnClickListener {
+            selectedJuego = "A"
+            highLightSelection(frameJuegoCount, frameJuegoCatch)
+        }
+
+        frameJuegoCount.setOnClickListener {
+            selectedJuego = "B"
+            highLightSelection(frameJuegoCatch, frameJuegoCount)
+        }
+
+        btnEmpezar.setOnClickListener {
+            when (selectedJuego) {
+
+                else -> Toast.makeText(this, "Elegir un juego, por favor.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun highLightSelection(selected: FrameLayout, other: FrameLayout) {
+
     }
 }
+
