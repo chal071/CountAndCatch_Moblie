4e925c5c2991c4aab8e8bfe48d5ba1cc34511ed1
Chenxiao Xu <cxu2223@politecnics.barcelona>
2025-11-23 22:09:19 +0100

    cambiar img

--- app/src/main/java/com/example/countandcatch/data/PairData.kt
+++ app/src/main/java/com/example/countandcatch/data/PairData.kt
@@ -5,11 +5,16 @@ import com.example.countandcatch.R
 
 object PairData {
     val pairMap = mapOf(
-        1 to listOf(R.drawable.dog1, R.drawable.hamster1, R.drawable.shark1),
-        2 to listOf(R.drawable.cat2, R.drawable.dog2, R.drawable.hamster2),
-        3 to listOf(R.drawable.dog3, R.drawable.duck3, R.drawable.shark3),
-        4 to listOf(R.drawable.cat4, R.drawable.duck4, R.drawable.shark4),
-        5 to listOf(R.drawable.cat5, R.drawable.duck5, R.drawable.hamster5)
+        1 to listOf(R.drawable.lion1, R.drawable.cat1, R.drawable.monky1,
+            R.drawable.dog1, R.drawable.watermelon1, R.drawable.orange1),
+        2 to listOf(R.drawable.lion2, R.drawable.cat2, R.drawable.monky2,
+            R.drawable.dog2, R.drawable.watermelon2, R.drawable.orange2),
+        3 to listOf(R.drawable.lion3, R.drawable.cat3, R.drawable.monky3,
+            R.drawable.dog3, R.drawable.watermelon3, R.drawable.orange3),
+        4 to listOf(R.drawable.lion4, R.drawable.cat4, R.drawable.monky4,
+            R.drawable.dog4, R.drawable.watermelon4, R.drawable.orange4),
+        5 to listOf(R.drawable.lion5, R.drawable.cat5, R.drawable.monky5,
+            R.drawable.dog5, R.drawable.watermelon5, R.drawable.orange5)
     )
 }
 
