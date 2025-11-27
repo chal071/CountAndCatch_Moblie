5743883dc7ed59df4e7e13ad935ebd2c7ac3c32a
Guillemmm7 <gruanom2324@politecnics.barcelona>
2025-11-27 15:55:51 +0100

    cambios pantalla elegir nivel

--- app/src/main/java/com/example/countandcatch/ElegirNivelJuego.kt
+++ app/src/main/java/com/example/countandcatch/ElegirNivelJuego.kt
@@ -2,9 +2,11 @@ package com.example.jocprojecteguillem
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.core.animate
 import com.example.countandcatch.R
 
 class MainActivity : AppCompatActivity() {
@@ -17,27 +19,56 @@ class MainActivity : AppCompatActivity() {
         val btnFacil = findViewById<Button>(R.id.btnJcFacil)
         val btnIntermedio = findViewById<Button>(R.id.btnJcIntermedio)
         val btnDificil = findViewById<Button>(R.id.btnJcDificil)
-        val btnContinuar = findViewById<Button>(R.id.btnJcContinuar)
+
+
+        val buttons = listOf(btnFacil, btnIntermedio, btnDificil)
 
         btnFacil.setOnClickListener {
+            resetZoom(buttons)
+            zoomSelected(it)
             nivelSeleccionado = "Fácil"
         }
 
         btnIntermedio.setOnClickListener {
+            resetZoom(buttons)
+            zoomSelected(it)
             nivelSeleccionado = "Intermedio"
         }
 
         btnDificil.setOnClickListener {
+            resetZoom(buttons)
+            zoomSelected(it)
             nivelSeleccionado = "Difícil"
         }
-
-        btnContinuar.setOnClickListener {
-            // Si no eligió nivel, mostramos aviso y salimos
-            if (nivelSeleccionado == null) {
-                Toast.makeText(this, "Selecciona un nivel antes de continuar", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-        }
-
     }
+    private fun addZoomEffect(button: View) {
+        button.animate()
+            .scaleX(1.15f)
+            .scaleY(1.15f)
+            .setDuration(150)
+            .withEndAction {
+                button.animate()
+                    .scaleX(1f)
+                    .scaleY(1f)
+                    .setDuration(150)
+            }
+    }
+
+    private fun zoomSelected(button: View) {
+        button.animate()
+            .scaleX(1.2f)
+            .scaleY(1.2f)
+            .setDuration(150)
+    }
+
+    private fun resetZoom(buttons: List<View>) {
+        buttons.forEach {
+            it.animate()
+                .scaleX(1f)
+                .scaleY(1f)
+                .setDuration(150)
+        }
+    }
+
+
 }
